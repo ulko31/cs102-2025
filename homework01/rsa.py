6cd714df7926dab23d1fa5fab55616b5f46b7f52
Козьменко Ульяна Сергеевна <uly.kozmenko@gmail.com>
2025-11-19 22:03:52 +0300

    Update rsa.py

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -14,7 +14,7 @@ def is_prime(n: int) -> bool:
     if n < 2:
         return False
     for i in range(2, int(n**0.5) + 1):
-B        if n % i == 0:
+        if n % i == 0:
             return False
     return True
 
