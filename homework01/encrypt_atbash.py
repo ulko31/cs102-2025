64156fcffef0d7c3d3f794773d66a159058cc1b8
ulanakozmenko <uly.kozmenko@gmail.com>
2026-01-14 10:10:16 +0300

    homework01

--- homework01/encrypt_atbash.py
+++ homework01/encrypt_atbash.py
@@ -1,13 +1,13 @@
 def encrypt_atbash(plaintext: str) -> str:
     result = []
     for ch in plaintext:
-        if 'a' <= ch <= 'z':
-            result.append(chr(ord('a') + (ord('z') - ord(ch))))
-        elif 'A' <= ch <= 'Z':
-            result.append(chr(ord('A') + (ord('Z') - ord(ch))))
+        if "a" <= ch <= "z":
+            result.append(chr(ord("a") + (ord("z") - ord(ch))))
+        elif "A" <= ch <= "Z":
+            result.append(chr(ord("A") + (ord("Z") - ord(ch))))
         else:
             result.append(ch)
-    return ''.join(result)
+    return "".join(result)
 
 if __name__ == "__main__":
     print(encrypt_atbash("Hello, World!"))
